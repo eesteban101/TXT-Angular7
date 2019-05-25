44da2da820a0edfe48329cf58f05211e15d19e01
eesteban101 <49514313+eesteban101@users.noreply.github.com>
2019-05-25 01:24:55 -0700

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public name = 'Eduardo Esteban';
+  public form: FormGroup;
+  // blogs start off empty
+  public blogs = [];
+
+  constructor(public _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group(
+      {
+        title: ['', Validators.required],
+        content: ['', Validators.required],
+      }
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    // add logic here to print out new blogs
+    // hint update your array of blogs;
+    this.blogs.push(this.form.value);
+  }
+}
